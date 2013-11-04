16f2fc6766fb8acc12230ad9efd4e87e07270b23
Will O <w.odonnell@gmail.com>
2013-11-03 18:10:41 -0600

    addr counter for address complement is working

--- PMBIST/SRC/pmbist/address_counter.v
+++ PMBIST/SRC/pmbist/address_counter.v
@@ -23,7 +23,7 @@ input                   s_in;
 input                   r_in;
 
 output  [tasw-1:0]      tas_out;
-reg     [tasw-1:0]      tas_out;
+wire    [tasw-1:0]      tas_out;
 
 //-----------------------------------------------
 // Internal Signals
@@ -46,94 +46,63 @@ reg     [tasw-1:0]      count;  // internal counter
 //  1  0 | 1  0 (addr set to first location)
 //  1  1 | 0  1 (addr set to last location)
 
-always @(posedge clk)
+always @(posedge clk) begin
   
   // Block in reset
   if (rst) begin
     count <= 0;
-    tas_out <= 0;
   
   // Set to first address
   end else if (s_in == 1) begin
     if (admd_in == `ADMD_LIUD) begin
         count <= `ADMD_LIUD_FIRST;
-        tas_out <= `ADMD_LIUD_FIRST;
     end else if (admd_in == `ADMD_PRUD) begin
         count <= `ADMD_PR_SEED;
-        tas_out <= `ADMD_PR_SEED;
     end 
   
   // Set to last address (reverse)
   end else if (r_in == 1) begin
     if (admd_in == `ADMD_LIUD) begin
         count <= `ADMD_LIUD_LAST;
-        tas_out <= `ADMD_LIUD_LAST;
     end else if (admd_in == `ADMD_PRUD) begin
         count <= `ADMD_PR_SEED;
-        tas_out <= `ADMD_PR_SEED;
     end 
   
   // Keep the address steady while tests run
   end else if (hold_in == 1) begin
     count <= count;
-    tas_out <= count;
 
   //-----------------------------------------------------------------
   // Handle Address Modes
   //-----------------------------------------------------------------
-  end else begin
-  case (admd_in) 
-    `ADMD_PRUD: begin
-        // LFSR implementation
-        // P(x) = (x^8)+(x^6)+(x^5)+(x^4)+1
-        if (updwn_in == 0) begin
-            count[7:1] <= count[6:0];   
-            count[0] <= (((count[7]^count[5])^count[4])^count[3]);
-            tas_out[7:1] <= count[6:0];   
-            tas_out[0] <= (((count[7]^count[5])^count[4])^count[3]);
-        end else begin
-            count[6:0] <= count[7:1];   
-            count[7] <= (((count[0]^count[5])^count[4])^count[3]);
-            tas_out[6:0] <= count[7:1];   
-            tas_out[7] <= (((count[0]^count[5])^count[4])^count[3]);
-        end
+  end else if (admd_in == `ADMD_PRUD) begin
+    // LFSR implementation
+    // P(x) = (x^8)+(x^6)+(x^5)+(x^4)+1
+    if (updwn_in == 0) begin
+        count[7:1] <= count[6:0];   
+        count[0] <= (((count[7]^count[5])^count[4])^count[3]);
+    end else begin
+        count[6:0] <= count[7:1];   
+        count[7] <= (((count[0]^count[5])^count[4])^count[3]);
     end
-
-    `ADMD_LIUD: begin
-        if(updwn_in == `ADDR_UP) begin
-            count <= count + 1;
-            tas_out <= count + 1;
-        end else begin
-            count <= count - 1;
-            tas_out <= count - 1;
-        end
+  
+  end else begin // all other modes use linear up/down counter
+    if(updwn_in == `ADDR_UP) begin
+        count <= count + 1;
+    end else begin
+        count <= count - 1;
     end
-    
-    `ADMD_AC: begin
-        // Perform the counter next value as blocking statement
-        if(updwn_in == `ADDR_UP) begin
-            count <= count + 1;
-        end else begin
-            count <= count - 1;
-        end
-        
-       // Only bit 7 is dependent on up/down
-        if (updwn_in == `ADDR_UP) begin
-            tas_out[7] <= count[0];
-        end else begin
-            tas_out[7] <= ~count[0];
-        end
-        // Transform the rest of the bits
-            tas_out[6] <= count[7]^count[0];
-            tas_out[5] <= count[6]^count[0];
-            tas_out[4] <= count[5]^count[0];
-            tas_out[3] <= count[4]^count[0];
-            tas_out[2] <= count[3]^count[0];
-            tas_out[1] <= count[2]^count[0];
-            tas_out[0] <= count[1]^count[0];
-    end
-    
-  endcase
+  end
 end 
- 
+
+
+assign tas_out[7] = (admd_in == `ADMD_AC) ? ((updwn_in == `ADDR_UP) ? count[0] : !count[0]) : count[7];
+assign tas_out[6] = (admd_in == `ADMD_AC) ? count[7]^count[0] : count[6];
+assign tas_out[5] = (admd_in == `ADMD_AC) ? count[6]^count[0] : count[5];
+assign tas_out[4] = (admd_in == `ADMD_AC) ? count[5]^count[0] : count[4];
+assign tas_out[3] = (admd_in == `ADMD_AC) ? count[4]^count[0] : count[3];
+assign tas_out[2] = (admd_in == `ADMD_AC) ? count[3]^count[0] : count[2];
+assign tas_out[1] = (admd_in == `ADMD_AC) ? count[2]^count[0] : count[1];
+assign tas_out[0] = (admd_in == `ADMD_AC) ? count[1]^count[0] : count[0];
+
 endmodule
